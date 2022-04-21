5fbab04015ef4c97fdb097a7296d779ad1663f43
cassiofb-dev <cassiofb.souza@gmail.com>
2022-04-21 20:55:13 -0300

    feat: new logic and prepare anylisis

--- tampdf/core/dataset.py
+++ tampdf/core/dataset.py
@@ -1,81 +1,87 @@
-import os, pandas as pd
+import os
 import numpy as np
-from urllib import request
+import pandas as pd
+import seaborn as sns
+
 from scipy import stats
+from urllib import request
+from pandas_profiling import ProfileReport
+
+sns.set_theme()
+
 
 class Dataset:
   def __init__(
     self,
+    url: str,
     name: str,
-    urls: list[str],
-    validation_urls: list[str] = [],
     generate_plots=True,
-    generate_profiles=True,
     generate_tables=True,
+    generate_profiles=True,
+    z_score_outlier_threshold: float = None,
   ) -> None:
+    self.url = url
     self.name = name
-    self.urls = urls
-    self.validation_urls = validation_urls
     self.generate_plots = generate_plots
-    self.generate_profiles = generate_profiles
     self.generate_tables = generate_tables
-    self.generic_paths: list[str] = []
-    self.validation_paths: list[str] = []
-    self.column_names = Dataset.get_column_names()
+    self.generate_profiles = generate_profiles
+    self.z_score_outlier_threshold = z_score_outlier_threshold
 
-    self.download_datasets()
+    self.initDataset()
 
-    self.generic_dataframe: pd.DataFrame = self.get_processed_data()
+  def initDataset(self):
+    self.download_dataset()
+    self.process_dataset()
+    self.save_dataset()
 
-    self.save_processed_data()
-
-  def download_datasets(self):
+  def download_dataset(self):
     os.makedirs(Dataset.get_raw_path(), exist_ok=True)
-    self.download_validation_or_generic_datasets(True, ".data")
-    self.download_validation_or_generic_datasets(False, "_validation.data")
 
-  def download_validation_or_generic_datasets(self, is_generic: bool, ends_with: str):
-    for (index, dataset_url) in enumerate(self.urls if is_generic else self.validation_urls):
-      DATASET_NAME = self.name + "_" + str(index) + ends_with
-      DATASET_PATH = os.path.join(Dataset.get_raw_path(), DATASET_NAME)
+    DATASET_NAME = self.name + ".data"
+    DATASET_PATH = os.path.join(Dataset.get_raw_path(), DATASET_NAME)
 
-      self.generic_paths.append(DATASET_PATH) if is_generic else self.validation_paths.append(DATASET_PATH)
+    self.path = DATASET_PATH
 
-      if os.path.isfile(DATASET_PATH) == False:
-        request.urlretrieve(dataset_url, DATASET_PATH)
+    if os.path.isfile(DATASET_PATH) == False:
+      request.urlretrieve(self.url, DATASET_PATH)
 
-  def generic_dataset_to_dataframe(self, generic_path: str) -> pd.DataFrame:
+  def dataset_to_dataframe(self, path: str) -> pd.DataFrame:
     raise "Not Implemented!"
 
-  def validation_dataset_to_dataframes(self, validation_path: str):
-    raise "Not Implemented!"
+  def process_dataset(self) -> pd.DataFrame:
+    dataframe = self.dataset_to_dataframe(self.path)
 
-  def generics_datasets_to_dataframes(self) -> list[pd.DataFrame]:
-    generic_dataframes: list[pd.DataFrame] = []
+    no_duplicate_df = dataframe.drop_duplicates()
 
-    for generic_path in self.generic_paths:
-      generic_dataframes.append(self.generic_dataset_to_dataframe(generic_path))
-
-    return generic_dataframes
-
-  def get_processed_data(self) -> pd.DataFrame:
-    generics_dataframes = self.generics_datasets_to_dataframes()
-
-    merged_generics_dataframes = pd.concat(generics_dataframes)
-
-    no_duplicate_df = merged_generics_dataframes.drop_duplicates()
+    if self.z_score_outlier_threshold == None:
+      self.dataframe = no_duplicate_df
+      return
 
     outlier_mask = (np.abs(stats.zscore(no_duplicate_df)) < 3).all(axis=1)
 
     no_outlier_df = no_duplicate_df[outlier_mask]
 
-    return no_outlier_df
+    self.dataframe = no_outlier_df
+    self.raw_dataframe = dataframe
 
-  def save_processed_data(self):
-    FILENAME = self.name + "_processed.csv"
+  def save_dataset(self):
+    os.makedirs(Dataset.get_processed_path(), exist_ok=True)
+
+    FILENAME = self.name + ".csv"
     PROCESSED_DATAPATH = os.path.join(Dataset.get_processed_path(), FILENAME)
 
-    self.generic_dataframe.to_csv(PROCESSED_DATAPATH)
+    if os.path.exists(PROCESSED_DATAPATH) == False:
+      self.dataframe.to_csv(PROCESSED_DATAPATH)
+
+    self.dataframe_path = PROCESSED_DATAPATH
+
+  def analyse(self):
+    if self.generate_plots: self.init_plot_analysis()
+    if self.generate_tables: self.init_plot_analysis()
+    if self.generate_profiles: self.init_plot_analysis()
+
+  def init_plot_analysis(self):
+    pass
 
   @staticmethod
   def get_ugriz_columns():
@@ -92,15 +98,39 @@ class Dataset:
   @staticmethod
   def get_raw_path():
     CURRENT_PATH = os.getcwd()
-    DATASET_PATH = "data/datasets/raws"
-    FULL_PATH = os.path.join(CURRENT_PATH, DATASET_PATH)
+    RAW_PATH = "data/datasets/raw"
+    FULL_PATH = os.path.join(CURRENT_PATH, RAW_PATH)
 
     return FULL_PATH
 
   @staticmethod
   def get_processed_path():
     CURRENT_PATH = os.getcwd()
-    DATASET_PATH = "data/datasets"
-    FULL_PATH = os.path.join(CURRENT_PATH, DATASET_PATH)
+    PROCESSED_PATH = "data/datasets/processed"
+    FULL_PATH = os.path.join(CURRENT_PATH, PROCESSED_PATH)
+
+    return FULL_PATH
+
+  @staticmethod
+  def get_plots_path():
+    CURRENT_PATH = os.getcwd()
+    PLOTS_PATH = "data/analysis/plots"
+    FULL_PATH = os.path.join(CURRENT_PATH, PLOTS_PATH)
+
+    return FULL_PATH
+
+  @staticmethod
+  def get_tables_path():
+    CURRENT_PATH = os.getcwd()
+    TABLES_PATH = "data/analysis/tables"
+    FULL_PATH = os.path.join(CURRENT_PATH, TABLES_PATH)
+
+    return FULL_PATH
+
+  @staticmethod
+  def get_profiles_path():
+    CURRENT_PATH = os.getcwd()
+    PROFILES_PATH = "data/analysis/profiles"
+    FULL_PATH = os.path.join(CURRENT_PATH, PROFILES_PATH)
 
     return FULL_PATH
